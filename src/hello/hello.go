7b3c6c27ad366aee53bf47763f9bf2d9238db674
namonak <namonak@gmail.com>
2019-07-28 21:29:53 +0900

    Added source directory.  - hello  - float

---
+++ src/hello/hello.go
@@ -0,0 +1,17 @@
+// hello.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num4 float32 = 1e7
+	var num5 float64 = .12345E+2
+	var num6 float64 = .532521E-2
+
+	fmt.Println(num4)
+	fmt.Println(num5)
+	fmt.Println(num6)
+	//fmt.Println(num7)
+}
